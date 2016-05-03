a8e96207ff6003f2911dd3eb624580be06b6fca7
noraeb <n.blomaard@gmail.com>
2016-05-03 18:05:38 +0200

    Created the board

--- src/components/BoardComponent.js
+++ src/components/BoardComponent.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import Paper from 'material-ui/lib/paper';
+
+const style = {
+  height: 100,
+  width: 100,
+  margin: 5,
+  textAlign: 'center',
+  display: 'inline-block',
+};
 
 class BoardComponent extends React.Component {
-  
+  render (){
+    return (
+      <div>
+        <div>
+          <Paper style={style} zDepth={1}/>
+          <Paper style={style} zDepth={1}/>
+          <Paper style={style} zDepth={1}/>
+        </div>
+        <div>
+          <Paper style={style} zDepth={1}/>
+          <Paper style={style} zDepth={1}/>
+          <Paper style={style} zDepth={1}/>
+        </div>
+        <div>
+          <Paper style={style} zDepth={1}/>
+          <Paper style={style} zDepth={1}/>
+          <Paper style={style} zDepth={1}/>
+        </div>
+      </div>
+        );
+    }
 }
 
 export default BoardComponent;
